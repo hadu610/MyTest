93e04633dbb3d649b8e84249c5fdc593a19f99d4
Du Ha <hadu610@gmail.com>
2014-06-10 15:29:58 +0700

    + Test Initial commit

---
+++ TheTest/TheTest/ASCViewController.h
@@ -0,0 +1,13 @@
+//
+//  ASCViewController.h
+//  TheTest
+//
+//  Created by Du Ha on 6/10/14.
+//  Copyright (c) 2014 ARISVN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASCViewController : UIViewController
+
+@end
---
+++ TheTest/TheTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TheTest
+//
+//  Created by Du Ha on 6/10/14.
+//  Copyright (c) 2014 ARISVN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ASCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ASCAppDelegate class]));
+    }
+}
